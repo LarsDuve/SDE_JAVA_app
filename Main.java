db7f327d2feb83a1132501cafa98dcce886b85d8
Senna2112 <raff0001@hz.nl>
2022-01-17 13:22:35 +0100

    added classes

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // table
+
+        // cards: 52
+
+        // rules: allowed cards
+
+        // players: minimal 1?
+
+        // play mode: turn based?
+    }
+
+}
